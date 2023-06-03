d1dbe5e9ff83191ecb23282937195b89c60efa84
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-06-04 03:33:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css"
+
+function App() {
+  return (
+    
+    // <Router>
+    // <div className="pt-20">
+      
+    // </div> 
+    // </Router>
+    <main>
+    <header>
+      <a href="">MyBlog</a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+    </main>
+  )
+}
+
+export default App
+
+// \\\  <Routes>
+//         <Route path="/" element={<Home />}/>
+//         <Route path="/about" element={<About />} />
+//       </Routes>
