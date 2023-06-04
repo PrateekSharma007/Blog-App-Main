d220b6e2a704ee5d8a0bd536814d4f8536ce6b42
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-06-05 02:22:41 +0530

    new

---
+++ src/Post.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+    <div className='entry'>
+    <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
+    <div className = "text">
+    <p className='title'>Electric Vehicle Benefits and Considerations</p>
+    <a href="" className="author">Prateek Sharma</a>
+    <time>2023-04-06</time>
+    <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet.</p></div>
+    
+  </div>
+
+  <div className='entry'>
+    <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
+    <div className = "text">
+    <p className='title'>Electric Vehicle Benefits and Considerations</p>
+    <a href="" className="author">Prateek Sharma  </a>
+    <time>2023-04-06</time>
+    <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet.</p></div>
+  </div>
+
+  <div className='entry'>
+    <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
+    <div className = "text">
+    <p className='title'>Electric Vehicle Benefits and Considerations</p>
+    <a href="" className="author">Prateek Sharma</a>
+    <time>2023-04-06</time>
+    <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet. </p></div>
+  </div>
+
+  <div className='entry'>
+    <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
+    <div className = "text">
+    <p className='title'>Electric Vehicle Benefits and Considerations</p>
+    <a href="" className="author">Prateek Sharma  </a>
+    <time>2023-04-06</time>
+    <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet.</p></div>
+    </div>
+    </div>
+  )
+}
+
+export default Post
