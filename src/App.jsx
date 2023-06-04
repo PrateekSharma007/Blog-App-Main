431593e6c648949bfc1e483e5b247ca681e37f99
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-06-05 01:47:29 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
+import "./components/Header"
+import Header from './components/Header';
 
 function App() {
   return (
@@ -11,37 +13,42 @@ function App() {
     // </div> 
     // </Router>
     <main>
-      <header>
-        <a href="">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
+      <Header/>
       <div className='entry'>
         <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
         <div className = "text">
         <p className='title'>Electric Vehicle Benefits and Considerations</p>
-        <p>The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet. The multiple fuel sources used to generate electricity results in a more secure energy source for the electrified portion of the transportation sector. All of this adds to our nation’s energy security.</p></div>
+        <a href="" className="author">Prateek Sharma</a>
+        <time>2023-04-06</time>
+        <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet.</p></div>
         
       </div>
 
       <div className='entry'>
         <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
         <div className = "text">
-        <p className='title'>Electric Vehicle Benefits and Considerations</p><p>The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet. The multiple fuel sources used to generate electricity results in a more secure energy source for the electrified portion of the transportation sector. All of this adds to our nation’s energy security.</p></div>
+        <p className='title'>Electric Vehicle Benefits and Considerations</p>
+        <a href="" className="author">Prateek Sharma  </a>
+        <time>2023-04-06</time>
+        <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet.</p></div>
       </div>
 
       <div className='entry'>
         <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
         <div className = "text">
-        <p className='title'>Electric Vehicle Benefits and Considerations</p><p>The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet. The multiple fuel sources used to generate electricity results in a more secure energy source for the electrified portion of the transportation sector. All of this adds to our nation’s energy security.</p></div>
+        <p className='title'>Electric Vehicle Benefits and Considerations</p>
+        <a href="" className="author">Prateek Sharma</a>
+        <time>2023-04-06</time>
+        <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet. </p></div>
       </div>
 
       <div className='entry'>
         <img src="https://s3-ap-southeast-1.amazonaws.com/subscriber.images/the-learning-tree/wp-content/uploads/2020/06/26091250/June20_SE_Electric-Car_Blog-Banner.jpg" alt="" />
         <div className = "text">
-        <p className='title'>Electric Vehicle Benefits and Considerations</p><p>The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet. The multiple fuel sources used to generate electricity results in a more secure energy source for the electrified portion of the transportation sector. All of this adds to our nation’s energy security.</p></div>
+        <p className='title'>Electric Vehicle Benefits and Considerations</p>
+        <a href="" className="author">Prateek Sharma  </a>
+        <time>2023-04-06</time>
+        <p classname = "info">The United States became a net exporter of petroleum in 2020 with exports surpassing imports, although imports of 7.86 million barrels per day remained an important part of balancing supply and demand for domestic and international markets. Overall, the transportation sector accounts for approximately 30% of total U.S. energy needs and 70% of U.S. petroleum consumption. Using more energy efficient vehicles like hybrid and electric vehicles supports the U.S. economy and helps diversify the U.S. transportation fleet.</p></div>
       </div>
     </main>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+    
+        <a href="">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Header;
