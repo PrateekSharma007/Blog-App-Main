965821c7106027c32d0f0e2223872e2eb6d489bb
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-06-11 15:55:17 +0530

    new

--- src/Pages/Registerpage.jsx
+++ src/Pages/Registerpage.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./register.css"
 
 const Registerpage = () => {
+  const [username,setUsername] = useState("") ; 
+  const [password , setPassword] = useState("");
+  async function register(e){
+    e.preventDefault() ;
+      await fetch('http://localhost:5000/register', {
+      method : "POST" ,
+      body : JSON.stringify({username,password}),
+      headers : {"Content-Type" : "application/json"}
+    })
+  }
+
   return (
-    <form className='register'>
-        <input type="text"  placeholder='username'/>
-        <input type="password" placeholder='password'/>
+    <form className='register' onSubmit={register}>
+        <input type="text"  placeholder='username' value = {username} onChange={e => setUsername(e.target.value)}/>
+        <input type="password" placeholder='password' value = {password} onChange={e => setPassword(e.target.value)}/>
         <button>Register</button>
     </form>
   )
--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,11 +1,15 @@
 const express = require('express') 
+const cors  = require("cors")
 const app = express() 
 
 
+app.use(cors())
+app.use(express.json())
+
+app.post("/register" , (req,res) =>{
+    const {username,password} = req.body ;
+    res.json({requestData: {username, password}})
+})
+
 
 app.listen(5000) 
-
-
-app.get("/" , (req,res) =>{
-    res.send("Home ")
-})
